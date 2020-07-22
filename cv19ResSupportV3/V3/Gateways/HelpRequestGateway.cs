7b2fb8efbeec03786d0cfa4552b80e2d3bbe96a7
gentex21 <mrpreston_99@yahoo.com>
2020-07-22 14:53:24 +0100

    Added a check for duplicate and set record status accordingly.

--- cv19ResSupportV3/V3/Gateways/HelpRequestGateway.cs
+++ cv19ResSupportV3/V3/Gateways/HelpRequestGateway.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using cv19ResSupportV3.V3.Gateways;
 using cv19ResSupportV3.V3.Infrastructure;
 using HelpRequest = cv19ResSupportV3.V3.Domain.HelpRequest;
@@ -17,9 +18,42 @@ namespace cv19ResSupportV3.V3.Gateways
 
         public int CreateHelpRequest(HelpRequestEntity request)
         {
-            _helpRequestsContext.HelpRequestEntities.Add(request);
-            _helpRequestsContext.SaveChanges();
-            return request.Id;
+            if (request != null)
+            {
+                SetRecordStatus(request);
+                _helpRequestsContext.HelpRequestEntities.Add(request);
+                _helpRequestsContext.SaveChanges();
+                return request.Id;
+            }
+            return 0;
+        }
+
+        private void SetRecordStatus(HelpRequestEntity request)
+        {
+            request.RecordStatus = "MASTER";
+            var duplicates = _helpRequestsContext.HelpRequestEntities
+                .Where(x => x.Uprn == request.Uprn && x.DobMonth == request.DobMonth
+                                                   && x.DobDay == request.DobDay && x.DobYear == request.DobYear &&
+                                                   x.ContactTelephoneNumber == request.ContactTelephoneNumber &&
+                                                   x.ContactMobileNumber == request.ContactMobileNumber).ToList();
+            if (duplicates.Count > 0)
+            {
+                foreach (var record in duplicates)
+                {
+                    var rec = _helpRequestsContext.HelpRequestEntities.Find(record.Id);
+                    rec.RecordStatus = "DUPLICATE";
+                }
+
+            }
+            else
+            {
+                var exceptions = _helpRequestsContext.HelpRequestEntities
+                    .Where(x => x.Uprn == request.Uprn).ToList();
+                if (exceptions.Count > 0)
+                {
+                    request.RecordStatus = "EXCEPTION";
+                }
+            }
         }
     }
 }
