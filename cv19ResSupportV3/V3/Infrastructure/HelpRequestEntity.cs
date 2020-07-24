d10eea750dc0f52a36b9e1ce60becab738b3b63c
gentex21 <mrpreston_99@yahoo.com>
2020-07-24 09:55:01 +0100

    Added some more data annotations.

--- cv19ResSupportV3/V3/Infrastructure/HelpRequestEntity.cs
+++ cv19ResSupportV3/V3/Infrastructure/HelpRequestEntity.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace cv19ResSupportV3.V3.Infrastructure
@@ -6,148 +7,149 @@ namespace cv19ResSupportV3.V3.Infrastructure
     [Table("i_need_help_resident_support_v3")]
     public class HelpRequestEntity
     {
-        [Column("id")]
+        [Column("id", TypeName = "int4")]
+        [Key]
         public int Id { get; set; }
 
-        [Column("is_on_behalf")]
+        [Column("is_on_behalf", TypeName = "boolean")]
         public bool IsOnBehalf { get; set; }
 
-        [Column("consent_to_complete_on_behalf")]
+        [Column("consent_to_complete_on_behalf", TypeName = "boolean")]
         public bool ConsentToCompleteOnBehalf { get; set; }
 
-        [Column("on_behalf_first_name")]
+        [Column("on_behalf_first_name", TypeName = "varchar")]
         public string OnBehalfFirstName { get; set; }
 
-        [Column("on_behalf_last_name")]
+        [Column("on_behalf_last_name", TypeName = "varchar")]
         public string OnBehalfLastName { get; set; }
 
-        [Column("on_behalf_email_address")]
+        [Column("on_behalf_email_address", TypeName = "varchar")]
         public string OnBehalfEmailAddress { get; set; }
 
-        [Column("on_behalf_contact_number")]
+        [Column("on_behalf_contact_number", TypeName = "varchar")]
         public string OnBehalfContactNumber { get; set; }
 
-        [Column("relationship_with_resident")]
+        [Column("relationship_with_resident", TypeName = "varchar")]
         public string RelationshipWithResident { get; set; }
 
-        [Column("postcode")]
+        [Column("postcode", TypeName = "varchar")]
         public string PostCode { get; set; }
 
-        [Column("uprn")]
+        [Column("uprn", TypeName = "varchar")]
         public string Uprn { get; set; }
 
-        [Column("ward")]
+        [Column("ward", TypeName = "varchar")]
         public string Ward { get; set; }
 
-        [Column("address_first_line")]
+        [Column("address_first_line", TypeName = "varchar")]
         public string AddressFirstLine { get; set; }
 
-        [Column("address_second_line")]
+        [Column("address_second_line", TypeName = "varchar")]
         public string AddressSecondLine { get; set; }
 
-        [Column("address_third_line")]
+        [Column("address_third_line", TypeName = "varchar")]
         public string AddressThirdLine { get; set; }
 
-        [Column("getting_in_touch_reason")]
+        [Column("getting_in_touch_reason", TypeName = "text")]
         public string GettingInTouchReason { get; set; }
 
-        [Column("help_with_accessing_food")]
+        [Column("help_with_accessing_food", TypeName = "boolean")]
         public bool HelpWithAccessingFood { get; set; }
 
-        [Column("help_with_accessing_medicine")]
+        [Column("help_with_accessing_medicine", TypeName = "boolean")]
         public bool HelpWithAccessingMedicine { get; set; }
 
-        [Column("help_with_accessing_other_essentials")]
+        [Column("help_with_accessing_other_essentials", TypeName = "boolean")]
         public bool HelpWithAccessingOtherEssentials { get; set; }
 
-        [Column("help_with_debt_and_money")]
+        [Column("help_with_debt_and_money", TypeName = "boolean")]
         public bool HelpWithDebtAndMoney { get; set; }
 
-        [Column("help_with_health")]
+        [Column("help_with_health", TypeName = "boolean")]
         public bool HelpWithHealth { get; set; }
 
-        [Column("help_with_mental_health")]
+        [Column("help_with_mental_health", TypeName = "boolean")]
         public bool HelpWithMentalHealth { get; set; }
 
-        [Column("help_with_accessing_internet")]
+        [Column("help_with_accessing_internet", TypeName = "boolean")]
         public bool HelpWithAccessingInternet { get; set; }
 
-        [Column("help_with_something_else")]
+        [Column("help_with_something_else", TypeName = "boolean")]
         public bool HelpWithSomethingElse { get; set; }
 
-        [Column("help_with_housing")]
+        [Column("help_with_housing", TypeName = "boolean")]
         public bool HelpWithHousing { get; set; }
 
-        [Column("help_with_jobs_or_training")]
+        [Column("help_with_jobs_or_training", TypeName = "boolean")]
         public bool HelpWithJobsOrTraining { get; set; }
 
-        [Column("help_with_children_and_schools")]
+        [Column("help_with_children_and_schools", TypeName = "boolean")]
         public bool HelpWithChildrenAndSchools { get; set; }
 
-        [Column("help_with_disabilities")]
+        [Column("help_with_disabilities", TypeName = "boolean")]
         public bool HelpWithDisabilities { get; set; }
 
-        [Column("medicine_delivery_help_needed")]
+        [Column("medicine_delivery_help_needed", TypeName = "boolean")]
         public bool MedicineDeliveryHelpNeeded { get; set; }
 
-        [Column("is_pharmacist_able_to_deliver")]
+        [Column("is_pharmacist_able_to_deliver", TypeName = "boolean")]
         public bool IsPharmacistAbleToDeliver { get; set; }
 
-        [Column("when_is_medicines_delivered")]
+        [Column("when_is_medicines_delivered", TypeName = "varchar")]
         public string WhenIsMedicinesDelivered { get; set; }
 
-        [Column("name_address_pharmacist")]
+        [Column("name_address_pharmacist", TypeName = "varchar")]
         public string NameAddressPharmacist { get; set; }
 
-        [Column("urgent_essentials")]
+        [Column("urgent_essentials", TypeName = "varchar")]
         public string UrgentEssentials { get; set; }
 
-        [Column("urgent_essentials_anything_else")]
+        [Column("urgent_essentials_anything_else", TypeName = "varchar")]
         public string UrgentEssentialsAnythingElse { get; set; }
 
-        [Column("current_support")]
+        [Column("current_support", TypeName = "varchar")]
         public string CurrentSupport { get; set; }
 
-        [Column("current_support_feedback")]
+        [Column("current_support_feedback", TypeName = "text")]
         public string CurrentSupportFeedback { get; set; }
 
-        [Column("first_name")]
+        [Column("first_name", TypeName = "varchar")]
         public string FirstName { get; set; }
 
-        [Column("last_name")]
+        [Column("last_name", TypeName = "varchar")]
         public string LastName { get; set; }
 
-        [Column("dob_month")]
+        [Column("dob_month", TypeName = "varchar")]
         public string DobMonth { get; set; }
 
-        [Column("dob_year")]
+        [Column("dob_year", TypeName = "varchar")]
         public string DobYear { get; set; }
 
-        [Column("dob_day")]
+        [Column("dob_day", TypeName = "varchar")]
         public string DobDay { get; set; }
 
-        [Column("contact_telephone_number")]
+        [Column("contact_telephone_number", TypeName = "varchar")]
         public string ContactTelephoneNumber { get; set; }
 
-        [Column("contact_mobile_number")]
+        [Column("contact_mobile_number", TypeName = "varchar")]
         public string ContactMobileNumber { get; set; }
 
-        [Column("email_address")]
+        [Column("email_address", TypeName = "varchar")]
         public string EmailAddress { get; set; }
 
-        [Column("gp_surgery_details")]
+        [Column("gp_surgery_details", TypeName = "text")]
         public string GpSurgeryDetails { get; set; }
 
-        [Column("number_of_children_under_18")]
+        [Column("number_of_children_under_18", TypeName = "varchar")]
         public string NumberOfChildrenUnder18 { get; set; }
 
-        [Column("consent_to_share")]
+        [Column("consent_to_share", TypeName = "boolean")]
         public bool ConsentToShare { get; set; }
 
-        [Column("date_time_recorded")]
+        [Column("date_time_recorded", TypeName = "timestamp")]
         public DateTime DateTimeRecorded { get; set; }
 
-        [Column("record_status")]
+        [Column("record_status", TypeName = "varchar")]
         public string RecordStatus { get; set; }
     }
 }
