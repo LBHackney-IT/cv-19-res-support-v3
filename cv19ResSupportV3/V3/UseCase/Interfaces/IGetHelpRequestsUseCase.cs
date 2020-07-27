6a9a4505995cb80f589e6e6f9725c9b126a472d6
gentex21 <mrpreston_99@yahoo.com>
2020-07-27 16:59:28 +0100

    New use case to get all help requests.

---
+++ cv19ResSupportV3/V3/UseCase/Interfaces/IGetHelpRequestsUseCase.cs
@@ -0,0 +1,9 @@
+using cv19ResSupportV3.V3.Boundary.Response;
+
+namespace cv19ResSupportV3.V3.UseCase
+{
+    public interface IGetHelpRequestsUseCase
+    {
+        HelpRequestResponseList Execute();
+    }
+}
