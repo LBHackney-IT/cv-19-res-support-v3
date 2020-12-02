2714341bf7b5090cf5255194cf1499ad7fc4ddb1
Maysa <maysa@madetech.com>
2020-12-02 11:41:22 +0000

    add nhs ctsas id
    Co-authored-by: Kat Katrina@madetech.com
    Co-authored-by: duslerke liudvikast@gmail.com

--- cv19ResSupportV3/V3/Domain/HelpRequest.cs
+++ cv19ResSupportV3/V3/Domain/HelpRequest.cs
@@ -61,5 +61,6 @@ namespace cv19ResSupportV3.V3.Domain
         public string CaseNotes { get; set; }
         public string AdviceNotes { get; set; }
         public string HelpNeeded { get; set; }
+        public string NhsCtasId { get; set; }
     }
 }
