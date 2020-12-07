80ed04c3e30a8f091b2755a3ba54901856ce2e67
LBHSPreston <selwyn.preston@hackney.gov.uk>
2020-12-07 12:21:18 +0000

    Add nhs number to help request context.

---
+++ cv19ResSupportV3/V3/Infrastructure/Migrations/20201207120525_AddNhsNumberToHelpRequestTable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace cv19ResSupportV3.V3.Infrastructure.Migrations
+{
+    public partial class AddNhsNumberToHelpRequestTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "nhs_number",
+                table: "i_need_help_resident_support_v3",
+                type: "character varying",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "nhs_number",
+                table: "i_need_help_resident_support_v3");
+        }
+    }
+}
