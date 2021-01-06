18c87ff122ad1cc262ed04c37507d271f53d20d2
Kat <katrina@madetech.com>
2021-01-06 11:32:57 +0000

    Add Request and Response boundaries for PatchHelpRequest

--- cv19ResSupportV3/V3/Controllers/HelpRequestsController.cs
+++ cv19ResSupportV3/V3/Controllers/HelpRequestsController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using cv19ResSupportV3.V3.Boundary.Requests;
 using cv19ResSupportV3.V3.Boundary.Response;
-using cv19ResSupportV3.V3.Domain;
 using cv19ResSupportV3.V3.Factories;
 using cv19ResSupportV3.V3.UseCase;
 using cv19ResSupportV3.V3.UseCase.Interfaces;
@@ -109,11 +108,12 @@ namespace cv19ResSupportV3.V3.Controllers
         /// <response code="400">There was an issue updating the record.</response>
         [HttpPatch]
         [Route("{id}")]
-        public IActionResult PatchHelpRequest([FromRoute] int id, [FromBody] HelpRequest request)
+        public IActionResult PatchHelpRequest([FromRoute] int id, [FromBody] HelpRequestPatchRequest request)
         {
             try
             {
-                _patchHelpRequestUseCase.Execute(id, request);
+                var domain = request.ToDomain();
+                _patchHelpRequestUseCase.Execute(id, domain);
                 return Ok();
             }
             catch (Exception e)
@@ -141,7 +141,7 @@ namespace cv19ResSupportV3.V3.Controllers
         /// </summary>
         /// <response code="200">Record retrieved successfully</response>
         /// <response code="404">A record with the specified id was not found</response>
-        [ProducesResponseType(typeof(HelpRequest), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(HelpRequestGetResponse), StatusCodes.Status200OK)]
         [HttpGet]
         [Route("{id}")]
         public IActionResult GetHelpRequest(int id)
