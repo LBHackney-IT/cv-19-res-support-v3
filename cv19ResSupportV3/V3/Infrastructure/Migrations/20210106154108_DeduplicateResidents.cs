9e9a34dd2427e7c30d9ccf744cca3b0eec3b84b9
LBHSPreston <selwyn.preston@hackney.gov.uk>
2021-01-07 09:40:09 +0000

    Added example queries on what might be needed to deduplicate.

--- cv19ResSupportV3/V3/Infrastructure/Migrations/20210106154108_DeduplicateResidents.cs
+++ cv19ResSupportV3/V3/Infrastructure/Migrations/20210106154108_DeduplicateResidents.cs
@@ -8,7 +8,14 @@ namespace cv19ResSupportV3.V3.Infrastructure.Migrations
         {
             //TODO:
             // 1. Find duplicate residents in the residents table (how do we identify duplicates)
-            // Match on first_name and last_name so far.....
+            // Match on first_name, last_name, dob_day and dob_year (dob_month has some issues)
+            // Distinct Records:
+            // select DISTINCT first_name, last_name, dob_day, dob_year from "public".i_need_help_resident_support_v3 order by last_name, first_name;
+            // Duplicate Matches:
+            // select first_name, last_name, dob_day, dob_year, count(*)
+            // from "public".i_need_help_resident_support_v3
+            // group by first_name, last_name, dob_day, dob_year
+            // HAVING count(*) > 1;
             // 2. For every duplicate resident find the master record
             // 3. Assign the help request for that resident to the master record
             // End result: Only records identified as master records will have associated help requests
