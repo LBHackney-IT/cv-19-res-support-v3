71d36ed08c1c0b8c4197212989d7aabf34097b0a
el <elena@madetech.com>
2021-01-20 14:02:26 +0000

    add residents table and migrate data (#70)
    * add residents table and migrate data
    
    * Added a help request table and relation to the resident
    
    Co-authored-by: el <elena@madetech.com>
    Co-authored-by: Kat <katrina@madetech.com>
    Co-authored-by: LBHSPreston <selwyn.preston@hackney.gov.uk>
    
    * added an empty migration file for deduplication and added some notes on how we want to go about running this script.
    
    * applied code formatting
    
    * Added example queries on what might be needed to deduplicate.
    
    * migrate and merge residents in migration
    
    * fix sql insert
    
    * updated the join evaluation to split temp_id field into an array for comparison
    
    * remove redundant migration
    
    * add temp_legacy_help_request_id to help_requests
    
    * remove redundant add help request migration and add resident case notes migration
    
    * renmae i_need_help_resident_support_v3 table to help_requests
    
    * drop reduntant coloumns from help_requests table
    
    * add resident id to help requests
    
    * drop case_notes from help_requests
    
    * remove temp coloumn from residents
    
    * lint
    
    * add resident_id foreign key
    
    Co-authored-by: Maysa <maysa@madetech.com>
    Co-authored-by: Kat <katrina@madetech.com>
    Co-authored-by: Maysa <maysa@madetech.com>
    Co-authored-by: Marten <marten@madetech.com>
    Co-authored-by: Maysa <maysa@madetech.com>
    
    * increase database connection timeouts
    
    Co-authored-by: Maysa <maysa@madetech.com>
    Co-authored-by: Kat <katrina@madetech.com>
    Co-authored-by: LBHSPreston <selwyn.preston@hackney.gov.uk>
    Co-authored-by: Marten <marten@madetech.com>

---
+++ cv19ResSupportV3/V3/Domain/Resident.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace cv19ResSupportV3.V3.Domain
+{
+    public class Resident
+    {
+        public int Id { get; set; }
+        public string PostCode { get; set; }
+        public string Uprn { get; set; }
+        public string Ward { get; set; }
+        public string AddressFirstLine { get; set; }
+        public string AddressSecondLine { get; set; }
+        public string AddressThirdLine { get; set; }
+        public bool? IsPharmacistAbleToDeliver { get; set; }
+        public string NameAddressPharmacist { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DobMonth { get; set; }
+        public string DobYear { get; set; }
+        public string DobDay { get; set; }
+        public string ContactTelephoneNumber { get; set; }
+        public string ContactMobileNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string GpSurgeryDetails { get; set; }
+        public string NumberOfChildrenUnder18 { get; set; }
+        public bool? ConsentToShare { get; set; }
+        public string CaseNotes { get; set; }
+        public string NhsNumber { get; set; }
+    }
+}
---
+++ cv19ResSupportV3/V3/Infrastructure/Migrations/20210112134042_DropReduntantColoumnsFromHelpRequestTable.cs
@@ -0,0 +1,59 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace cv19ResSupportV3.V3.Infrastructure.Migrations
+{
+    public partial class DropReduntantColoumnsFromHelpRequestTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "first_name", table: "help_requests");
+            migrationBuilder.DropColumn(name: "last_name", table: "help_requests");
+            migrationBuilder.DropColumn(name: "dob_day", table: "help_requests");
+            migrationBuilder.DropColumn(name: "dob_month", table: "help_requests");
+            migrationBuilder.DropColumn(name: "dob_year", table: "help_requests");
+            migrationBuilder.DropColumn(name: "contact_mobile_number", table: "help_requests");
+            migrationBuilder.DropColumn(name: "contact_telephone_number", table: "help_requests");
+            migrationBuilder.DropColumn(name: "email_address", table: "help_requests");
+            migrationBuilder.DropColumn(name: "address_first_line", table: "help_requests");
+            migrationBuilder.DropColumn(name: "address_second_line", table: "help_requests");
+            migrationBuilder.DropColumn(name: "address_third_line", table: "help_requests");
+            migrationBuilder.DropColumn(name: "postcode", table: "help_requests");
+            migrationBuilder.DropColumn(name: "uprn", table: "help_requests");
+            migrationBuilder.DropColumn(name: "ward", table: "help_requests");
+            migrationBuilder.DropColumn(name: "is_pharmacist_able_to_deliver", table: "help_requests");
+            migrationBuilder.DropColumn(name: "name_address_pharmacist", table: "help_requests");
+            migrationBuilder.DropColumn(name: "gp_surgery_details", table: "help_requests");
+            migrationBuilder.DropColumn(name: "number_of_children_under_18", table: "help_requests");
+            migrationBuilder.DropColumn(name: "consent_to_share", table: "help_requests");
+            migrationBuilder.DropColumn(name: "record_status", table: "help_requests");
+            migrationBuilder.DropColumn(name: "nhs_number", table: "help_requests");
+            migrationBuilder.DropColumn(name: "case_notes", table: "help_requests");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(name: "first_name", table: "help_requests", nullable: true);
+            migrationBuilder.AddColumn<string>(name: "last_name", table: "help_requests", nullable: true);
+            migrationBuilder.AddColumn<string>(name: "dob_day", table: "help_requests", nullable: true);
+            migrationBuilder.AddColumn<string>(name: "dob_month", table: "help_requests", nullable: true);
+            migrationBuilder.AddColumn<string>(name: "dob_year", table: "help_requests", nullable: true);
+            migrationBuilder.AddColumn<string>(name: "contact_mobile_number", table: "help_requests", nullable: true);
+            migrationBuilder.AddColumn<string>(name: "contact_telephone_number", table: "help_requests", nullable: true);
+            migrationBuilder.AddColumn<string>(name: "email_address", table: "help_requests", nullable: true);
+            migrationBuilder.AddColumn<string>(name: "address_first_line", table: "help_requests", nullable: true);
+            migrationBuilder.AddColumn<string>(name: "address_second_line", table: "help_requests", nullable: true);
+            migrationBuilder.AddColumn<string>(name: "address_third_line", table: "help_requests", nullable: true);
+            migrationBuilder.AddColumn<string>(name: "postcode", table: "help_requests", nullable: true);
+            migrationBuilder.AddColumn<string>(name: "uprn", table: "help_requests", nullable: true);
+            migrationBuilder.AddColumn<string>(name: "ward", table: "help_requests", nullable: true);
+            migrationBuilder.AddColumn<bool>(name: "is_pharmacist_able_to_deliver", table: "help_requests", nullable: true);
+            migrationBuilder.AddColumn<string>(name: "name_address_pharmacist", table: "help_requests", nullable: true);
+            migrationBuilder.AddColumn<string>(name: "gp_surgery_details", table: "help_requests", nullable: true);
+            migrationBuilder.AddColumn<string>(name: "number_of_children_under_18", table: "help_requests", nullable: true);
+            migrationBuilder.AddColumn<bool>(name: "consent_to_share", table: "help_requests", nullable: true);
+            migrationBuilder.AddColumn<string>(name: "record_status", table: "help_requests", nullable: true);
+            migrationBuilder.AddColumn<string>(name: "nhs_number", table: "help_requests", nullable: true);
+            migrationBuilder.AddColumn<string>(name: "case_notes", table: "help_requests", nullable: true);
+        }
+    }
+}
