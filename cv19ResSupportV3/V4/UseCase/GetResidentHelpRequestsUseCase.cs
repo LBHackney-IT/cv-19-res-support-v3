210e6cbdd40226b4094ecd2d9e441ee05426d779
LBHSPreston <selwyn.preston@hackney.gov.uk>
2021-01-25 16:15:22 +0000

    use case to get a collection of help requests for a resident (not yet implemented)

---
+++ cv19ResSupportV3/V4/UseCase/GetResidentHelpRequestsUseCase.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using cv19ResSupportV3.V4.Boundary.Response;
+using cv19ResSupportV3.V4.UseCase.Interface;
+
+namespace cv19ResSupportV3.V4.UseCase
+{
+    public class GetResidentHelpRequestsUseCase : IGetResidentHelpRequestsUseCase
+    {
+        public List<ResidentHelpRequestResponse> Execute(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ cv19ResSupportV3/V4/UseCase/Interface/IGetResidentHelpRequestsUseCase.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using cv19ResSupportV3.V4.Boundary.Response;
+
+namespace cv19ResSupportV3.V4.UseCase.Interface
+{
+    public interface IGetResidentHelpRequestsUseCase
+    {
+        List<ResidentHelpRequestResponse> Execute(int id);
+    }
+}
