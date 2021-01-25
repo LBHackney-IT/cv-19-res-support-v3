a6745bf7182e238ae56d48dbaab1dfc768705ceb
LBHSPreston <selwyn.preston@hackney.gov.uk>
2021-01-25 14:48:31 +0000

    create residents use case added to respond to the create controller action.

---
+++ cv19ResSupportV3/V4/UseCase/Interface/ICreateResidentsUseCase.cs
@@ -0,0 +1,7 @@
+namespace cv19ResSupportV3.V4.UseCase.Interface
+{
+    public interface ICreateResidentsUseCase
+    {
+        ResidentResponseBoundary Execute(ResidentRequestBoundary request);
+    }
+}
