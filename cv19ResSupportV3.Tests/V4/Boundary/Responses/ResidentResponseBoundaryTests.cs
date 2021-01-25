d95419ca330cd2b6226205e67d81e09ef7c4caaa
LBHSPreston <selwyn.preston@hackney.gov.uk>
2021-01-25 14:52:00 +0000

    added tests for the new residents feature.

---
+++ cv19ResSupportV3.Tests/V4/Boundary/Responses/ResidentResponseBoundaryTests.cs
@@ -0,0 +1,41 @@
+using AutoFixture;
+using cv19ResSupportV3.V4;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace cv19ResSupportV3.Tests.V4.Boundary.Requests
+{
+    [TestFixture]
+    public class ResidentResponseBoundaryTests
+    {
+        [Test]
+        public void ResidentRequestBoundaryShouldHaveTheCorrectProperties()
+        {
+            var entityType = typeof(ResidentResponseBoundary);
+            entityType.GetProperties().Length.Should().Be(22);
+            var entity = new Fixture().Create<ResidentResponseBoundary>();
+            Assert.That(entity, Has.Property("Id").InstanceOf(typeof(int)));
+            Assert.That(entity, Has.Property("FirstName").InstanceOf(typeof(string)));
+            Assert.That(entity, Has.Property("LastName").InstanceOf(typeof(string)));
+            Assert.That(entity, Has.Property("DobDay").InstanceOf(typeof(string)));
+            Assert.That(entity, Has.Property("DobMonth").InstanceOf(typeof(string)));
+            Assert.That(entity, Has.Property("DobYear").InstanceOf(typeof(string)));
+            Assert.That(entity, Has.Property("ContactTelephoneNumber").InstanceOf(typeof(string)));
+            Assert.That(entity, Has.Property("ContactMobileNumber").InstanceOf(typeof(string)));
+            Assert.That(entity, Has.Property("EmailAddress").InstanceOf(typeof(string)));
+            Assert.That(entity, Has.Property("AddressFirstLine").InstanceOf(typeof(string)));
+            Assert.That(entity, Has.Property("AddressSecondLine").InstanceOf(typeof(string)));
+            Assert.That(entity, Has.Property("AddressThirdLine").InstanceOf(typeof(string)));
+            Assert.That(entity, Has.Property("Postcode").InstanceOf(typeof(string)));
+            Assert.That(entity, Has.Property("Uprn").InstanceOf(typeof(string)));
+            Assert.That(entity, Has.Property("Ward").InstanceOf(typeof(string)));
+            Assert.That(entity, Has.Property("IsPharmacistAbleToDeliver").InstanceOf(typeof(bool?)));
+            Assert.That(entity, Has.Property("NameAddressPharmacist").InstanceOf(typeof(string)));
+            Assert.That(entity, Has.Property("GpSurgeryDetails").InstanceOf(typeof(string)));
+            Assert.That(entity, Has.Property("NumberOfChildrenUnder18").InstanceOf(typeof(string)));
+            Assert.That(entity, Has.Property("ConsentToShare").InstanceOf(typeof(bool?)));
+            Assert.That(entity, Has.Property("RecordStatus").InstanceOf(typeof(string)));
+            Assert.That(entity, Has.Property("NhsNumber").InstanceOf(typeof(string)));
+        }
+    }
+}
---
+++ cv19ResSupportV3.Tests/V4/E2ETests/GetResident.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using AutoFixture;
+using cv19ResSupportV3.Tests.V3.E2ETests;
+using cv19ResSupportV3.Tests.V3.Helpers;
+using cv19ResSupportV3.V3.Boundary.Response;
+using cv19ResSupportV3.V3.Boundary.Requests;
+using cv19ResSupportV3.V3.Domain;
+using cv19ResSupportV3.V3.Infrastructure;
+using cv19ResSupportV3.V4;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace cv19ResSupportV3.Tests.V4.E2ETests
+{
+    [TestFixture]
+    public class GetResident : IntegrationTests<Startup>
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            DatabaseContext.Database.RollbackTransaction();
+            E2ETestHelpers.ClearTable(DatabaseContext);
+        }
+
+        [Test]
+        public async Task CreateResidentRequestCreatesRecord()
+        {
+            var resident = new Fixture().Build<ResidentEntity>()
+                .Without(re => re.HelpRequests)
+                .Without(re => re.CaseNotes)
+                .Create();
+            DatabaseContext.ResidentEntities.Add(resident);
+            DatabaseContext.SaveChanges();
+            var uri = new Uri($"api/v4/residents/{resident.Id}", UriKind.Relative);
+            var response = Client.GetAsync(uri);
+            var statusCode = response.Result.StatusCode;
+            statusCode.Should().Be(200);
+            var content = response.Result.Content;
+            var stringContent = await content.ReadAsStringAsync().ConfigureAwait(true);
+            var convertedResponse = JsonConvert.DeserializeObject<ResidentResponseBoundary>(stringContent);
+            var residentEntity = DatabaseContext.ResidentEntities.FirstOrDefault();
+            //replace with a full object comparison once we have a resident toresponse factory method
+            convertedResponse.FirstName.Should().Be(residentEntity.FirstName);
+            convertedResponse.LastName.Should().Be(residentEntity.LastName);
+        }
+    }
+}
