db5e9aba1e13103d31e4cbd1fba4f1a830b05d01
LBHSPreston <selwyn.preston@hackney.gov.uk>
2021-01-25 16:14:44 +0000

    use case to get a single help request for a resident

---
+++ cv19ResSupportV3/V4/UseCase/GetResidentHelpRequestUseCase.cs
@@ -0,0 +1,13 @@
+using cv19ResSupportV3.V4.Boundary.Response;
+using cv19ResSupportV3.V4.UseCase.Interface;
+
+namespace cv19ResSupportV3.V4.UseCase
+{
+    public class GetResidentHelpRequestUseCase : IGetResidentHelpRequestUseCase
+    {
+        public ResidentHelpRequestResponse Execute(int id, int helpRequestId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
