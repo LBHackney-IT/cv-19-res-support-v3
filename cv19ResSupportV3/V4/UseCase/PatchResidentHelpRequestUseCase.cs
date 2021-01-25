864e18a6d67c3b45efd4e640c1286f945e1399e4
LBHSPreston <selwyn.preston@hackney.gov.uk>
2021-01-25 16:24:52 +0000

    use case to patch a help requests for a resident (not yet implemented)

---
+++ cv19ResSupportV3/V4/UseCase/PatchResidentHelpRequestUseCase.cs
@@ -0,0 +1,14 @@
+using cv19ResSupportV3.V4.Boundary.Requests;
+using cv19ResSupportV3.V4.Boundary.Response;
+using cv19ResSupportV3.V4.UseCase.Interface;
+
+namespace cv19ResSupportV3.V4.UseCase
+{
+    public class PatchResidentHelpRequestUseCase : IPatchResidentHelpRequestUseCase
+    {
+        public ResidentHelpRequestResponse Execute(int id, int helpRequestId, ResidentHelpRequestRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
