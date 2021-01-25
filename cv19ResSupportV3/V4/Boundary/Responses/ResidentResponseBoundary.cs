4ae6cce6f5bb7dcdfa1d6855bcd2c5b498cd6e54
LBHSPreston <selwyn.preston@hackney.gov.uk>
2021-01-25 14:51:37 +0000

    new resident request and response boundary objects

---
+++ cv19ResSupportV3/V4/Boundary/Responses/ResidentResponseBoundary.cs
@@ -0,0 +1,28 @@
+namespace cv19ResSupportV3.V4
+{
+    public class ResidentResponseBoundary
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DobDay { get; set; }
+        public string DobMonth { get; set; }
+        public string DobYear { get; set; }
+        public string ContactTelephoneNumber { get; set; }
+        public string ContactMobileNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string AddressFirstLine { get; set; }
+        public string AddressSecondLine { get; set; }
+        public string AddressThirdLine { get; set; }
+        public string Postcode { get; set; }
+        public string Uprn { get; set; }
+        public string Ward { get; set; }
+        public bool? IsPharmacistAbleToDeliver { get; set; }
+        public string NameAddressPharmacist { get; set; }
+        public string GpSurgeryDetails { get; set; }
+        public string NumberOfChildrenUnder18 { get; set; }
+        public bool? ConsentToShare { get; set; }
+        public string RecordStatus { get; set; }
+        public string NhsNumber { get; set; }
+    }
+}
