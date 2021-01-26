7965b44f44f4e7c97d6fd9a582178991e6f77fef
LBHSPreston <selwyn.preston@hackney.gov.uk>
2021-01-26 10:42:44 +0000

    add endpoint to search residents

--- cv19ResSupportV3/V4/Controllers/ResidentsController.cs
+++ cv19ResSupportV3/V4/Controllers/ResidentsController.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
 using cv19ResSupportV3.V3.Controllers;
+using cv19ResSupportV3.V3.Domain.Commands;
+using cv19ResSupportV3.V4.Boundary.Requests;
 using cv19ResSupportV3.V4.UseCase.Interface;
 using cv19ResSupportV3.V4.UseCase.Interfaces;
 using Microsoft.AspNetCore.Http;
@@ -18,15 +21,18 @@ namespace cv19ResSupportV3.V4.Controllers
         private readonly ICreateResidentsUseCase _createResidentsUseCase;
         private readonly IGetResidentsUseCase _getResidentsUseCase;
         private readonly IPatchResidentUseCase _patchResidentsUseCase;
+        private readonly ISearchResidentsUseCase _searchResidentsUseCase;
 
         public ResidentsController(
             ICreateResidentsUseCase createResidentsUseCase,
             IGetResidentsUseCase getResidentsUseCase,
-            IPatchResidentUseCase patchResidentsUseCase)
+            IPatchResidentUseCase patchResidentsUseCase,
+            ISearchResidentsUseCase searchResidentsUseCase)
         {
             _createResidentsUseCase = createResidentsUseCase;
             _getResidentsUseCase = getResidentsUseCase;
             _patchResidentsUseCase = patchResidentsUseCase;
+            _searchResidentsUseCase = searchResidentsUseCase;
         }
 
 
@@ -58,6 +64,19 @@ namespace cv19ResSupportV3.V4.Controllers
             if (response != null)
                 return Ok(response);
             return (NotFound("Resident not found"));
+        }
+
+        /// <summary>
+        /// Gets a resident with the id specified.
+        /// </summary>
+        /// <response code="200">...</response>
+        /// <response code="404">...</response>
+        [ProducesResponseType(typeof(List<ResidentResponseBoundary>), StatusCodes.Status200OK)]
+        [HttpGet]
+        public IActionResult SearchResidents([FromQuery] FindResident requestParams)
+        {
+            var response = _searchResidentsUseCase.Execute(requestParams);
+            return Ok(response);
         }
 
         /// <summary>
