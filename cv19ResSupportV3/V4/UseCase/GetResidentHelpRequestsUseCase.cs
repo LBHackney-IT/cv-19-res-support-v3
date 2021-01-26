776a75bc912b268a2177b90406a61d5c8d38c213
LBHSPreston <selwyn.preston@hackney.gov.uk>
2021-01-26 08:28:16 +0000

    implemented the use case to get resident help requests

--- cv19ResSupportV3/V4/UseCase/GetResidentHelpRequestsUseCase.cs
+++ cv19ResSupportV3/V4/UseCase/GetResidentHelpRequestsUseCase.cs
@@ -1,14 +1,24 @@
 using System.Collections.Generic;
+using cv19ResSupportV3.V3.Gateways;
 using cv19ResSupportV3.V4.Boundary.Response;
+using cv19ResSupportV3.V4.Factories;
 using cv19ResSupportV3.V4.UseCase.Interface;
 
 namespace cv19ResSupportV3.V4.UseCase
 {
     public class GetResidentHelpRequestsUseCase : IGetResidentHelpRequestsUseCase
     {
+        private readonly IHelpRequestGateway _helpRequestGateway;
+        public GetResidentHelpRequestsUseCase(IHelpRequestGateway helpRequestGateway)
+        {
+            _helpRequestGateway = helpRequestGateway;
+        }
         public List<ResidentHelpRequestResponse> Execute(int id)
         {
-            throw new System.NotImplementedException();
+            var residentHelpRequests = _helpRequestGateway.GetResidentHelpRequests(id);
+            return residentHelpRequests == null
+                ? new List<ResidentHelpRequestResponse>()
+                : residentHelpRequests.ToResidentHelpRequestResponse();
         }
     }
 }
