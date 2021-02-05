844912cda5b42b22e49ff06462af7143ec0ae227
Kat <katrina@madetech.com>
2021-02-05 16:15:10 +0000

    Add Get Case Notes by Resident Id Use Case

--- cv19ResSupportV3/V4/Boundary/Responses/CaseNoteResponse.cs
+++ cv19ResSupportV3/V4/Boundary/Responses/CaseNoteResponse.cs
@@ -3,8 +3,8 @@ namespace cv19ResSupportV3.V4.Boundary.Responses
     public class CaseNoteResponse
     {
         public int Id { get; set; }
-        public int ResidentId { get; set; }
-        public int HelpRequestId { get; set; }
-        public string CaseNote { get; set; }
+            public int ResidentId { get; set; }
+            public int HelpRequestId { get; set; }
+            public string CaseNote { get; set; }
     }
 }
