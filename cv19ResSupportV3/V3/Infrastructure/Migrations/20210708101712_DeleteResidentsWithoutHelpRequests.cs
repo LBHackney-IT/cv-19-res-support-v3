989a745f9088195d73359aa843335ecb0ab6391d
Owen Pigott <owen.pigott@madetech.com>
2021-07-08 13:00:33 +0100

    Added Delete script to clear residents with no help requests and case notes.

---
+++ cv19ResSupportV3/V3/Infrastructure/Migrations/20210708101712_DeleteResidentsWithoutHelpRequests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace cv19ResSupportV3.V3.Infrastructure.Migrations
+{
+    public partial class DeleteResidentsWithoutHelpRequests : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+              @"DELETE FROM residents
+                WHERE id NOT IN (select resident_id FROM help_requests)
+                AND id NOT IN (select resident_id FROM resident_case_notes);"
+            );
+        }
+    }
+}
