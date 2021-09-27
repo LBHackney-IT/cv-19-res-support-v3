33bc05547cdcfe605e73299772bbd265f7c462fe
Rachel Hutchinson <32848419+rachel-hutchinson@users.noreply.github.com>
2021-09-27 15:39:46 +0100

    Update help needed subtype (#130)
    * Merge Dev to Master - Add HelpNeededSubtype (#127)
    
    * Add HelpNeededSubtype column to HelpRequest table
    
    * Auto-formatting.
    
    * Revert startup file change
    
    Co-authored-by: Owen Pigott <owen.pigott@madetech.com>
    
    * Additional Updates for HelpNeededSubtype
    
    Co-authored-by: Owen Pigott <owen.pigott@madetech.com>

--- cv19ResSupportV3/V4/Boundary/Responses/ResidentHelpRequestResponse.cs
+++ cv19ResSupportV3/V4/Boundary/Responses/ResidentHelpRequestResponse.cs
@@ -44,6 +44,7 @@ namespace cv19ResSupportV3.V4.Boundary.Response
         public string CaseNotes { get; set; }
         public string AdviceNotes { get; set; }
         public string HelpNeeded { get; set; }
+        public string HelpNeededSubtype { get; set; }
         public string NhsCtasId { get; set; }
         public string AssignedTo { get; set; }
         public dynamic Metadata { get; set; }
