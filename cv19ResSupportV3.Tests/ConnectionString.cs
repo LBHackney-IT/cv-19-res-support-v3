cb82cfdc508dd260c46a878aa45791101b587754
Owen Pigott <owen.pigott@madetech.com>
2021-10-06 13:09:09 +0100

    Reverted test DB connection details

--- cv19ResSupportV3.Tests/ConnectionString.cs
+++ cv19ResSupportV3.Tests/ConnectionString.cs
@@ -7,10 +7,10 @@ namespace cv19ResSupportV3.Tests
         public static string TestDatabase()
         {
             return $"Host={Environment.GetEnvironmentVariable("DB_HOST") ?? "127.0.0.1"};" +
-                   $"Port={Environment.GetEnvironmentVariable("DB_PORT") ?? "5433"};" +
+                   $"Port={Environment.GetEnvironmentVariable("DB_PORT") ?? "5432"};" +
                    $"Username={Environment.GetEnvironmentVariable("DB_USERNAME") ?? "postgres"};" +
                    $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "mypassword"};" +
-                   $"Database={Environment.GetEnvironmentVariable("DB_DATABASE") ?? "devdb"}";
+                   $"Database={Environment.GetEnvironmentVariable("DB_DATABASE") ?? "testdb"}";
         }
     }
 }
